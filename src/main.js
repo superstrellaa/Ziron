a40bbe80edd54efd2e2ae306fdafa844a25a8c2a
superstrella <superstrellayt@gmail.com>
2026-04-10 17:06:12 +0200

    feat: add eye cursor at flyCamera use, add lucide import and add CREDITS

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
 import "./style.css";
+import { createIcons } from "lucide";
 import { createViewport } from "./editor/viewport.js";
 
 document.querySelector("#app").innerHTML = `
   <div id="toolbar">
+    <i data-lucide="box"></i>
     <span>ZIRON</span>
   </div>
   <div id="viewport"></div>
 `;
 
+/* createIcons({
+  icons: {  },
+  attrs: {
+    width: 16,
+    height: 16,
+    stroke: "#a78bfa",
+  },
+});
+ */
 createViewport(document.getElementById("viewport"));
